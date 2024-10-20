5347643314d77c92528727901c337edac5573602
Nic <nicholas.t.hirt@gmail.com>
2024-10-20 13:28:00 -0700

    Created a custom Checkbox component. Next goal is to create a dropdown component for features such Ascendancy selection or playstyle?

--- src/app/builds/page.tsx
+++ src/app/builds/page.tsx
@@ -1,10 +1,13 @@
 import Header from "@/components/Header"
 import CardHolder from "@/components/CardHolder"
+import Filter from "@/components/Filter";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header/>
+      <div className="h-14" />
+      <Filter></Filter>
       <div className="grid gap-4 mt-10 px-80">
         <CardHolder className="grid-cols-2 mt-10"/>
       </div>
